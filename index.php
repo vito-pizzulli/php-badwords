cd64e8331d773950c655b26df9a1d581bb2d1435
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-30 14:25:04 +0200

    created the form for data entry into index.php

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
     <title>PHP Badwords Form</title>
 </head>
 <body>
-    <h1>Ciao</h1>
+    <h1>PHP Badwords</h1>
+    <form action="./server.php" type="GET">
+        <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci il testo"></textarea>
+        <input type="text" name="word-to-censor" id="word-to-censor" placeholder="Inserisci la parola da censurare">
+        <button type="submit">Conferma</button>
+    </form>
 </body>
 </html>
