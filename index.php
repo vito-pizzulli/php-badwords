73f93e1015e0b3c318f2cd87a9f3cb01ec36fd15
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-30 15:24:59 +0200

    imported style.css & google font VT323 in index.php, all html put into main

--- index.php
+++ index.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords Form</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Google Fonts VT323 Regular 400 -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>PHP Badwords</h1>
-    <form action="./server.php" type="GET">
-        <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci il testo"></textarea>
-        <input type="text" name="word-to-censor" id="word-to-censor" placeholder="Inserisci la parola da censurare">
-        <button type="submit">Conferma</button>
-    </form>
+    <main>
+        <h1>PHP Badwords</h1>
+        <form action="./server.php" type="GET">
+            <textarea name="paragraph" id="paragraph" cols="30" rows="10" placeholder="Inserisci il testo"></textarea>
+            <input type="text" name="word-to-censor" id="word-to-censor" placeholder="Inserisci la parola da censurare">
+            <button type="submit">Conferma</button>
+        </form>
+    </main>
 </body>
 </html>
