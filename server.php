a3dd1b20a64ad838dec88916cf373098bd7d1f61
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-30 15:25:47 +0200

    imported style.css & google font VT323 in server.php, all html put into main, fixed second echo (now displaying the censored paragraph length)

--- server.php
+++ server.php
@@ -9,22 +9,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords Server</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Google Fonts VT323 Regular 400 -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>PHP Badwords</h1>
-
-    <h2>Paragrafo inserito e sua lunghezza:</h2>
-    <span>
-    <?php
-        echo $text . " " . strlen($text);
-    ?>
-    </span>
-
-    <h2>Paragrafo inserito (con parola scelta censurata) e sua lunghezza:</h2>
-    <span>
-    <?php
-        echo str_replace($word, '***', $text) . " " . strlen($text);
-    ?>
-    </span>
+    <main>
+        <h1>PHP Badwords</h1>
+        <h2>Paragrafo inserito - Lunghezza</h2>
+        <p>
+            <?php
+                echo $text . " - " . strlen($text);
+            ?>
+        </p>
+        <h2>Paragrafo inserito (censurato) - Lunghezza</h2>
+        <p>
+            <?php
+            $censoredText = str_replace($word, '***', $text);
+                echo $censoredText . " - " . strlen($censoredText);
+            ?>
+        </p>
+    </main>
 </body>
 </html>
